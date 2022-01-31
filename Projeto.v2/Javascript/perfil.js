10ee1e8e69cdcaae67e31c2bab5ee37384ed6502
DiegoDSousa <80205634+DiegoDSousa@users.noreply.github.com>
2022-01-31 15:42:03 -0100

    atualizando perfil 2.0
    atualizando o perfil na branch pesquisa

--- Projeto.v2/Javascript/perfil.js
+++ Projeto.v2/Javascript/perfil.js
@@ -2,20 +2,40 @@
 import nomes from './informacao.class.js';
 
 console.log(localStorage.nome)
-var a=localStorage.getItem("nome")
-var conteudo=document.getElementById("teste")
-console.log(a)
-conteudo.innerText=a
+var nome=localStorage.getItem("nome")
+console.log(nome)
 
 var i=0
 while(i<=7){
-    if(a==nomes[i]){
+    if(nome==nomes[i]){
         break
     }
     i=i+1
 }
+var topo=document.getElementById('topo')
+
+
+var nomeUsuario=document.createElement('P')
+var descrisao=document.createElement('P')
+var texto=document.createElement('DIV')
 var perfil=document.createElement('IMG')
+
 var foto=document.getElementById('topo')
+
+topo.classList.add("header")
+descrisao.classList.add("descricao")
+texto.classList.add("conteudo")
+
+
+
+
+texto.append(nomeUsuario,descrisao)
 foto.append(perfil)
+topo.append(texto)
+
+
 
 perfil.src="../Imagens/Imagensperfilpequenas/perfil"+ i +".png"
+perfil.alt="foto perfil do usuario"
+descrisao.innerHTML="breve descricao sobre o sujeito"
+nomeUsuario.innerText=nome
