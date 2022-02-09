4b01fae73e1af96e6f55ebc2fbd5054a0ded463e
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-02-09 10:47:46 -0100

    enviar messagem css pronto

--- Projeto.v2/Javascript/enviar_messagens.js
+++ Projeto.v2/Javascript/enviar_messagens.js
@@ -1,7 +1,6 @@
 var box_messagens=document.getElementById("box_messagens")
 function criar_perfil(){
 
-
 var box_perfil=document.getElementById("box_perfil")
 
 var user=document.createElement('DIV')
@@ -31,4 +30,75 @@ user_description.append(nome,description)
 user.append(user_icon,user_description)
 box_perfil.append(user)
 }
+
+function criar_messagens(){
+    var box_messagens=document.getElementById("box_messagens")
+
+    var conversa=[["R","Gostaria de saber quando, organiza-ra a proxima excursão ao monte cara."],
+                ["E","A proxima excursão sera realizada no dia 24 de Janeiro."],
+                ["E","Mas gosta de mergulhos?"],
+                ["R","Sim"],
+                ["E","Porem na proxima semana, havera um mergulho na enseada de coral"],
+                ["R","Quando posso marcar?"],
+                ["E","Colocarei o anúncio na segunda feira"],
+                ["E","=============================="],
+                ["R","============================="],
+                ["E","================================"],
+                ["E","=============================="],
+                ["R","============================="],
+                ["E","================================"],
+                ["E","=============================="],
+                ["R","============================="],
+                ["E","================================"]
+            ]
+
+
+    var tamanho=conversa.length
+    for(let i=0;i<tamanho;i++){
+
+        var messagem=document.createElement('DIV')
+        var user_icon=document.createElement('DIV')
+        var foto=document.createElement('IMG')
+        var content=document.createElement('DIV')
+        var text=document.createElement('SPAM')
+        
+        
+        
+        if (conversa[i][0]=="R"){
+            messagem.classList.add("box_msg_R")
+            foto.src="../Imagens/perfil/perfil0.jpg"
+            text.innerHTML=conversa[i][1]
+
+            user_icon.append(foto)
+            content.append(text)
+            messagem.append(user_icon,content)
+            box_messagens.append(messagem)
+        }else{
+            messagem.classList.add("box_msg_E")
+            foto.src="../Imagens/perfil/perfil1.jpg"
+            text.innerHTML=conversa[i][1]
+
+            user_icon.append(foto)
+            content.append(text)
+            messagem.append(content,user_icon)
+            box_messagens.append(messagem)
+        }
+
+        user_icon.classList.add("user_icon_m")
+        content.classList.add("box_content")
+
+
+    
+       
+        content.append(text)
+        messagem.append(user_icon,content)
+        box_messagens.append(messagem)
+
+    }
+    
+}
+
+
+
 criar_perfil()
+criar_messagens()
