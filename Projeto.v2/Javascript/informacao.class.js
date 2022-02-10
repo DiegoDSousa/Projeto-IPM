f9f3d6a022d7d48dbbabaff1507725e83ef2c7c6
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-02-10 21:05:22 -0100

    dicionario usuarios

--- Projeto.v2/Javascript/informacao.class.js
+++ Projeto.v2/Javascript/informacao.class.js
@@ -1,4 +1,33 @@
-var nomes=["João Antonio","Raquel Maria","Rafael Antonio","Mario Andrade","Rafaela Simone",
-"Rita Perreira","Guilherme Reis","Helena Gomes"]
+var usuarios=[
+    {
+        "nome":"Nuno Lima",
+        "descricao":"Sem nada a dizer",
+        "imagens":"nuno",
+        "numero_fotos":4,
+        "id":1,
+    },  {
+        "nome":"Diego Sousa",
+        "descricao":"Sem nada a dizer",
+        "imagens":"diego",
+        "numero_fotos":5,
+        "id":2,
+    },  {
+        "nome":"Leticia Zego",
+        "descricao":"Sem nada a dizer",
+        "imagens":"Leticia",
+        "numero_fotos":3,
+        "id":3,
+    },  {
+        "nome":"William Carvalho",
+        "descricao":"Sem nada a dizer",
+        "imagens":"willi",
+        "numero_fotos":3,
+        "id":4,
+    }
+
+
+
+
+]
 
 export default nomes 
