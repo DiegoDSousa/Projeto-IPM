7791d92a3a996acb59d7a286f2a3b6a059db5f53
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-02-11 08:34:27 -0100

    css pesquisa

--- Projeto.v2/Javascript/pesquisa.js
+++ Projeto.v2/Javascript/pesquisa.js
@@ -48,6 +48,7 @@ function redirecionar(){
 
 
 function pesquisar(){
+     
     document.getElementById("lista").innerHTML=''
     var nomePesquisado=document.getElementById("pesquisar").value
     nomePesquisado=nomePesquisado.toLocaleLowerCase()
@@ -86,3 +87,39 @@ function usuarioNaoEncontrado(){
     
 }
 
+function criar_perfil(){
+
+    var pesquisado=document.getElementById("lista")
+    
+    var user=document.createElement('DIV')
+    var user_icon=document.createElement('DIV')
+    var user_description=document.createElement('DIV')
+    var foto=document.createElement('IMG')
+    var user_status=document.createElement('DIV')
+    var nome=document.createElement('P')
+    var description=document.createElement('P')
+    
+    
+    foto.classList.add("perfil_icon")
+    user_status.classList.add("user_status")
+    user.classList.add("user_box")
+    user_description.classList.add("user_description")
+    user_icon.classList.add("user_icon")
+    description.classList.add("description")
+    nome.classList.add("nome")
+    
+    
+    foto.src="../Imagens/usuarios/"+usuarios[1].imagens+"/perfil.jpg"
+    user_status.innerHTML=""
+    nome.innerText=nomeusuario
+    description.innerHTML="Sem nada a dizer"
+    
+    
+    user_icon.append(foto,user_status)
+    user_description.append(nome,description)
+    user.append(user_icon,user_description)
+    pesquisado.append(user)
+    }
+  
+
+criar_perfil()
