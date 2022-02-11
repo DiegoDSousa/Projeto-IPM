0ecc9e7eddef2dada81ddf408cc085d8f3949e97
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2022-02-11 05:56:26 -0100

    feed css pronto

--- Projeto.v2/Javascript/feed.js
+++ Projeto.v2/Javascript/feed.js
@@ -1,57 +1,59 @@
 var i = 0;
 import usuarios from './informacao.class.js';
 function criar(){  
-    var feed=document.getElementById('feed')
-    var scrol=document.getElementById('conteudo')
+    var conteudo=document.getElementById('conteudo')
 
-    var publicacao=document.createElement('DIV')
-    var header=document.createElement('DIV')
-    var perfil=document.createElement('IMG')
+    var post=document.createElement('DIV')
+    var user=document.createElement('DIV')
+    var user_description=document.createElement('DIV')
+    var user_icon=document.createElement('DIV')
+    var user_perfil=document.createElement('IMG')
     var nome=document.createElement('P')
-    var icone=document.createElement('P')
-    var icone_img=document.createElement('IMG')
-    var footer=document.createElement('DIV')
-    var foto_publicada=document.createElement('IMG')
-    var div_foto=document.createElement('DIV')
-    var comentar=document.createElement('IMG')
-    var partilhar=document.createElement('IMG')
-    var gosto=document.createElement('IMG')
-    var divisao=document.createElement('HR')
+    var data=document.createElement('P')
+    var post_img_box=document.createElement('DIV')
+    var post_img=document.createElement('IMG')
+    var rection_box=document.createElement('DIV')
+    var like=document.createElement('IMG')
+    var share=document.createElement('IMG')
+    var coment=document.createElement('IMG')
 
-    scrol.classList.add("conteudo")
-    header.classList.add("header")
-    publicacao.classList.add("publicacao")
-    foto_publicada.classList.add("foto_publicada")
-    div_foto.classList.add("foto")
-    perfil.classList.add("perfil")
-    icone_img.classList.add("editar")
+//Class
+    post.classList.add("post_box")
+    user.classList.add("user_box")
+    user_description.classList.add("user_des")
+    user_perfil.classList.add("perfil_icon")
     nome.classList.add("nome")
+    data.classList.add("data")
+    post_img_box.classList.add("post_post_img_box")
+    post_img.classList.add("post_img")
+    rection_box.classList.add("rection_box")
+    user_icon.classList.add("user_icon")
+    like.classList.add("rection")
+    share.classList.add("rection")
+    coment.classList.add("rection")
 
-    feed.append(scrol)
+//ligações
+    user_icon.append(user_perfil)
+    user_description.append(nome,data)
+    user.append(user_icon,user_description)
+    post_img_box.append(post_img)
+    rection_box.append(like,share,coment)
+    post.append(user,post_img_box,rection_box)
+    conteudo.append(post)
     nome.append(usuarios[i].nome)
-    scrol.append(publicacao)
-    icone.append(icone_img)
-    header.append(perfil,nome,icone)
-    div_foto.append(foto_publicada)
-    footer.append(gosto,comentar,partilhar)
-    publicacao.append(header)
-    publicacao.append(div_foto)
-    publicacao.append(footer)
-    publicacao.append(divisao)
+    data.append("Hoje")
 
-    nome.setAttribute("onclick","abrirPerfil()")
+    user.setAttribute("onclick","abrirPerfil()")
 
-    partilhar.alt="partilhar"
-    perfil.alt="foto_perfil"
-    gosto.alt="gosto"
-    comentar.alt="comentar"
-
-    perfil.src="../Imagens/usuarios/"+usuarios[i].imagens+"/perfil.jpg"
-    foto_publicada.src="../Imagens/usuarios/"+usuarios[i].imagens+"/img"+j +".jpg"
-    icone_img.src="../Imagens/Icones/editar.svg"
-    gosto.src="../Imagens/Icones/Gosto.svg"
-    comentar.src="../Imagens/Icones/comentario.svg"
-    partilhar.src="../Imagens/Icones/partilhar.svg"
+    share.alt="partilhar"
+    user_perfil.alt="foto_perfil"
+    like.alt="gosto"
+    coment.alt="comentar"
+    user_perfil.src="../Imagens/usuarios/"+usuarios[i].imagens+"/perfil.jpg"
+    post_img.src="../Imagens/usuarios/"+usuarios[i].imagens+"/img"+j +".jpg"
+    like.src="../Imagens/Icones/Gosto.svg"
+    coment.src="../Imagens/Icones/comentario.svg"
+    share.src="../Imagens/Icones/partilhar.svg"
 
 
 }  
